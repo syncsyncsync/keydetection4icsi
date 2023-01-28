6be8926f9069f85646a6bbe94f8c32f328a51ca2
syncsyncsync <syncsyncsync>
2023-01-28 07:24:21 +0000

    train error

--- keypoint.py
+++ keypoint.py
@@ -3,8 +3,7 @@
 #
 # Keypoint Detection on ICSI Dataset
 #
-# Akira Okutomi <okutomi@pm.me>
-#
+
 # 2020-12-12: Added data augmentation
 
 
@@ -34,10 +33,25 @@ from detectron2.data.datasets import register_coco_instances
 from detectron2.data import transforms as T
 
 setup_logger()
+K50_1 = "COCO-Keypoints/keypoint_rcnn_R_50_FPN_1x"
+K50_3 = "COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x"
+K101 = "COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x"
+KX101 = "COCO-Keypoints/keypoint_rcnn_X_101_32x8d_FPN_3x"
+PRETRAIN_WEIGHT_NAME = K50_1
 
-K101 = 'ICSI-DETECTION/SP_keypoint_rcnn_R_101_FPN_3x.yaml'
+K50_3_path = "COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml"
+K101_path = "COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x.yaml"
+DETECT_config1 = "ICSI-DETECTION/SP_keypoint_rcnn_R_101_FPN_3x.yaml"
+DETECT_config2 = "ICSI-DETECTION/SP_keypoint_rcnn_R_50_FPN_3x.yaml"
+TRAIN_WEIGHT_CONF = K50_3_path
 
-#K50 = "./configs/COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml"
+
+
+#  DATA K101_3
+BASE_DIR = '/home/ssm-user/detectron2/20230104keypoints'
+LABEL_DIR = "annotations/person_keypoints_default.json"
+IMAGE_DIR = "images"
+
 _SMALL_OBJECT_AREA_THRESH = 1000
 _LARGE_MASK_AREA_THRESH = 120000
 _OFF_WHITE = (1.0, 1.0, 240.0 / 255)
@@ -143,7 +157,9 @@ def register_dataset(name, annotaitions, images, force=False):
 def init_keypoints(DATA_SET_NAME,
                    fullpath_coco_json=None,
                    fullpath_coco_images=None,
-                   config_file=K101,
+                   fullpath_coco_train_json=None,
+                   fullpath_coco_train_images=None,
+                   config_file=TRAIN_WEIGHT_CONF,
                    output_dir="./output",
                    mode="train"):
 
@@ -158,14 +174,16 @@ def init_keypoints(DATA_SET_NAME,
 
     if mode == "train":
         DatasetCatalog.clear()
-        if fullpath_coco_json:
-            assert os.path.exists(fullpath_coco_json), \
-            "*** ArgumentCheckErorr Annotations not found at {}".format(fullpath_coco_json)
-        if fullpath_coco_images:
-            assert os.path.exists(fullpath_coco_images), \
-            "*** ArgumentCheckErorr Annotations not found at {}".format(fullpath_coco_images)
+
+
+        if not fullpath_coco_train_json:
+            assert os.path.exists(fullpath_coco_train_json), \
+            "*** ArgumentCheckErorr Annotations not found at {}".format(fullpath_coco_train_json)
+        if not fullpath_coco_train_images:
+            assert os.path.exists(fullpath_coco_train_images), \
+            "*** ArgumentCheckErorr Annotations not found at {}".format(fullpath_coco_train_images)
         
-        register_dataset(DATA_SET_NAME, fullpath_coco_json, fullpath_coco_images)
+        register_dataset(DATA_SET_NAME, fullpath_coco_train_json, fullpath_coco_train_images)
 
     # classes = MetadataCatalog.get("sp09").thing_classes = ["sperm_head","Sperm"]
     # MetadataCatalog.get("sp09").thing_classes = ["sperm_head","Sperm"]
@@ -187,63 +205,6 @@ def init_keypoints(DATA_SET_NAME,
     # Keypoint Head
     # ---------------------------------------------------------------------------- #
     cfg.OUTPUT_DIR = output_dir
-    # Directory where output files are written
-    # Set seed to negative to fully randomize everything.
-    # Detail:
-    # Set seed to positive to use a fixed seed. Note that a fixed seed increases
-    # reproducibility but does not guarantee fully deterministic behavior.
-    # Disabling all parallelism further increases reproducibility.
-    #cfg.SEED = -1  
-
-    # Benchmark different cudnn algorithms.
-    # If input images have very different sizes, this option will have large overhead for about 10k iterations. 
-    # It usually hurts total time, but can benefit for certain models.
-    # If input images have the same or similar sizes, benchmark is often helpful.
-    #cfg.CUDNN_BENCHMARK = False
-
-    # The period (in terms of steps) for minibatch visualization at train time.
-    # Set to 0 to disable.
-    #cfg.VIS_PERIOD = 0
-
-    # ---------------------------------------------------------------------------- #
-    # Keypoint Head
-    # ---------------------------------------------------------------------------- #
-    #cfg.MODEL.ROI_KEYPOINT_HEAD.NAME = "KRCNNConvDeconvUpsampleHead"
-    #cfg.MODEL.ROI_KEYPOINT_HEAD.POOLER_RESOLUTION = 14
-    #cfg.MODEL.ROI_KEYPOINT_HEAD.POOLER_SAMPLING_RATIO = 0
-    #cfg.MODEL.ROI_KEYPOINT_HEAD.CONV_DIMS = tuple(512 for _ in range(8))
-    #cfg.MODEL.ROI_KEYPOINT_HEAD.NUM_KEYPOINTS = 4
-    
-    # Images with too few (or no) keypoints are excluded from training.
-    #cfg.MODEL.ROI_KEYPOINT_HEAD.MIN_KEYPOINTS_PER_IMAGE = 1 
-
-    # This should be a list[float] with the same length as ROI_KEYPOINT_HEAD.NUM_KEYPOINTS, in our case 4.
-    #cfg.TEST.KEYPOINT_OKS_SIGMAS = np.ones((4, 1), dtype=float).tolist()
-
-    # Type of pooling operation applied to the incoming feature map for each RoI
-    #cfg.MODEL.ROI_KEYPOINT_HEAD.POOLER_TYPE = "ROIAlignV2"
-
-    # Normalize by the total number of visible keypoints in the minibatch if True.
-    # Otherwise, normalize by the total number of keypoints that could ever exist
-    # in the minibatch.
-    # The keypoint softmax loss is only calculated on visible keypoints.
-    # Since the number of visible keypoints can vary significantly between
-    # minibatches, this has the effect of up-weighting the importance of
-    # minibatches with few visible keypoints. (Imagine the extreme case of
-    # only one visible keypoint versus N: in the case of N, each one
-    # contributes 1/N to the gradient compared to the single keypoint
-    # determining the gradient direction). Instead, we can normalize the
-    # loss by the total number of keypoints, if it were the case that all
-    # keypoints were visible in a full minibatch. (Returning to the example,
-    # this means that the one visible keypoint contributes as much as each
-    # of the N keypoints.)
-    #cfg.MODEL.ROI_KEYPOINT_HEAD.NORMALIZE_LOSS_BY_VISIBLE_KEYPOINTS = True
-    # Multi-task loss weight to use for keypoints
-    # Recommended values:
-    #   - use 1.0 if NORMALIZE_LOSS_BY_VISIBLE_KEYPOINTS is True
-    #   - use 4.0 if NORMALIZE_LOSS_BY_VISIBLE_KEYPOINTS is False
-    #cfg.MODEL.ROI_KEYPOINT_HEAD.LOSS_WEIGHT = 1.0
-
     return cfg
 
 # To verify the data loading is correct
@@ -265,18 +226,18 @@ def cv2_imshow(im):
     im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
     plt.figure(figsize=(25, 8)), plt.imshow(im), plt.axis('off')
 
-def ressume_train(DATA_SET_NAME, cfg, model_weight, config_file=K101):
+def ressume_train(DATA_SET_NAME, cfg, model_weight, config_file=TRAIN_WEIGHT_CONF):
     print("model_weight:", model_weight)
     print("config_file:", config_file)
 
     assert os.path.exists(model_weight), "Model weight not found at {}".format(model_weight)
     assert os.path.exists(config_file), "Config file not found at {}".format(config_file)
 
-    train(model_weight,cfg, resume=True ,config_file=K101)
+    train(model_weight,cfg, resume=True ,config_file=TRAIN_WEIGHT_CONF)
     return cfg
 
 
-def train(cfg, train_dataset=None, test_dataset=None ,resume=False, model_weight=None ,config_file=K101):
+def train(cfg, train_dataset=None, test_dataset=None ,resume=False, model_weight=None ,config_file=TRAIN_WEIGHT_CONF):
     # args:
     #
     # config_file: config file  for keypoints detection
@@ -286,44 +247,73 @@ def train(cfg, train_dataset=None, test_dataset=None ,resume=False, model_weight
     # model_weight = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
     #cfg.merge_from_file(model_zoo.get_config_file(config_file))
     
+    cfg.merge_from_file(model_zoo.get_config_file(config_file))
+    #cfg.MODEL.DEVICE = "cpu"
+    cfg.MODEL.DEVICE = "cuda"
+    
     if model_weight == None:
-        if os.path.exists(cfg.MODEL.WEIGHTS):
-            model_weight = cfg.MODEL.WEIGHTS
-        else:
-            # print error and exit 
-            assert os.path.exists(cfg.MODEL.WEIGHTS), "Model weight not found at {}".format(cfg.MODEL.WEIGHTS)
-
+        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(K50_3)
+        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(config_file)
+        #assert os.path.exists(cfg.MODEL.WEIGHTS), "Model weight not found at {}".format(cfg.MODEL.WEIGHTS)
+        #cfg.MODEL.WEIGHTS = "detectron2://COCO-Detection/faster_rcnn_R_101_FPN_3x/137851257/model_final_f6e8b1.pkl"
     else:        
         if os.path.exists(model_weight):
             cfg.MODEL.WEIGHTS = model_weight
         else:
-            print("Model weight not found at {}".format(model_weight))
-            # exit error if cfg.MODEL.WEIGHTS is not exist
-            assert os.path.exists(model_weight), "Model weight not found at {}".format(model_weight)
-
-    cfg.MODEL.DEVICE = "cuda"
+            pass
 
     if train_dataset != None:
-        cfg.DATASETS.TRAIN = (train_dataset,)
+        cfg.DATASETS.TRAIN = (train_dataset)
+    else:
+        cfg.DATASETS.TRAIN = (DATA_SET_NAME)
 
     if test_dataset != None:
-        cfg.DATASETS.TEST = (test_dataset,)
+        cfg.DATASETS.TEST = (test_dataset)
+    else:        
+        cfg.DATASETS.TEST = (DATA_SET_NAME,)
     # cfg.DATASETS.TEST = ("hand_test",)  #Dataset 'hand_test' is empty in my case
     #cfg.DATALOADER.NUM_WORKERS = 12
         
     os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 
+
+    # Images with too few (or no) keypoints are excluded from training.
+    cfg.MODEL.ROI_KEYPOINT_HEAD.MIN_KEYPOINTS_PER_IMAGE = 32
+    # Type of pooling operation applied to the incoming feature map for each RoI
+    cfg.MODEL.ROI_KEYPOINT_HEAD.POOLER_TYPE = "ROIAlignV2"
+    #  - use 4.0 if NORMALIZE_LOSS_BY_VISIBLE_KEYPOINTS is False
+    cfg.MODEL.ROI_KEYPOINT_HEAD.LOSS_WEIGHT = 1.0
+    cfg.MODEL.ROI_KEYPOINT_HEAD.NUM_KEYPOINTS = 4
+    cfg.MODEL.KEYPOINT_ON = True
+    cfg.MODEL.ROI_KEYPOINT_HEAD.NUM_KEYPOINTS = 4
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
+    
+    cfg.TEST.KEYPOINT_OKS_SIGMAS = np.ones((4, 1), dtype=float).tolist()
+    
+    cfg.DATASETS.TEST = ()
+    cfg.DATALOADER.NUM_WORKERS = 4
+    cfg.SOLVER.IMS_PER_BATCH = 8
+    cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+    cfg.SOLVER.MAX_ITER = 90000
+    cfg.SOLVER.CHECKPOINT_PERIOD = 5000
+    cfg.SOLVER.MAX_ITER = 100000
+    #cfg.SOLVER.IMS_PER_BATCH = 64
+    
+    
+
     #trainer = DefaultTrainer(cfg)    #CocoTrainer(cfg)
     trainer = MyTrainer(cfg)    #CocoTrainer(cfg)
     trainer.resume_or_load(resume)
     trainer.train()
+
     return cfg
     
 
 def predict(cfg,
             image_path,
             model_weight,
-            config_file=K101,
+            config_file=DETECT_config1,
             sample=10,
             output_dir="pred",
             device="cuda"
@@ -360,6 +350,7 @@ def predict(cfg,
     
     # override config by args
     cfg.MODEL.WEIGHTS = model_weight
+    cfg.MODEL.KEYPOINT_ON = True
     cfg.MODEL.DEVICE = device
     cfg.TEST.KEYPOINT_OKS_SIGMAS = np.array([1,2,2,2], dtype=float).tolist()    
     cfg.OUTPUT_DIR = output_dir
@@ -407,30 +398,35 @@ def on_image(image_path, outputs_dir, predictor):
     v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
     cv2.imwrite(os.path.join(outputs_dir, os.path.basename(image_path)), v.get_image()[:, :, ::-1])
 
-
-def start_train():
-    BASE_DIR = '/home/ssm-user/detectron2/20230104keypoints'
-    LABEL_DIR = "annotations/person_keypoints_default.json"
-    IMAGE_DIR = "images"
+def train_from_zero():
+    
     DATA_SET_NAME = "sp12"
     #print("Base_dir:", BASE_DIR)
     cfg = init_keypoints(DATA_SET_NAME,
                          fullpath_coco_train_json=os.path.join(BASE_DIR, LABEL_DIR),
                          fullpath_coco_train_images=os.path.join(BASE_DIR, IMAGE_DIR))
-    train(DATA_SET_NAME, cfg, resume=True, model_weight="model_final.pth", config_file=K101)
-
+    train(cfg, train_dataset=None, test_dataset=None , resume=False, model_weight=None, config_file=PRETRAIN_WEIGHT_NAME)
+    
+def start_train():
+    DATA_SET_NAME = "sp12"
+    #print("Base_dir:", BASE_DIR)
+    cfg = init_keypoints(DATA_SET_NAME,
+                         fullpath_coco_train_json=os.path.join(BASE_DIR, LABEL_DIR),
+                         fullpath_coco_train_images=os.path.join(BASE_DIR, IMAGE_DIR))
+    train(cfg, resume=True,  model_weight="model_final.pth", config_file=TRAIN_WEIGHT_CONF)
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--mode', type=str, default="detect", help='train,resume, detect')
-    parser.add_argument('--basedir', type=str, default='/home/ssm-user/detectron2/20230104keypoints', help='base dir')
+    parser.add_argument('--mode', type=str, default="train", help='train,resume, detect')
+    parser.add_argument('--basedir', type=str, default=BASE_DIR, help='base dir')
     parser.add_argument('--dataset', type=str, default='sp12', help='dataset name')
     parser.add_argument('--interactive', type=bool, default=True, help='interactive mode')
     parser.add_argument('--weight', type=str, default='model_final.pth', help='model weight')
     parser.add_argument('--output', type=str, default='output', help='output dir')
     parser.add_argument('--source', type=str, default="20230109keypoints_test/images", help='input source dir (test imaeges dir)')
     parser.add_argument('--sample', type=int, default=0, help='sample number')
-    parser.add_argument('--config', type=str, default='COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x.yaml', help='config file')
-    parser.add_argument('--label', type=str, default="annotations/person_keypoints_default.json", help='label name relative to base dir')
+    parser.add_argument('--config', type=str, default=TRAIN_WEIGHT_CONF, help='config file')
+    parser.add_argument('--label', type=str, default=LABEL_DIR, help='label name relative to base dir')
     parser.add_argument('--image', type=str, default="annotations/images", help='training images dir')
 
     args = parser.parse_args()
@@ -470,7 +466,7 @@ if __name__ == "__main__":
         predict(cfg,
                 image_path,
                 model_weight=DEFAULT_WEIGHT,
-                config_file=K101,
+                config_file=DETECT_config2,
                 sample=SAMPLES,
                 output_dir=OUTPUT_DIR, device="cpu")
 
@@ -479,10 +475,10 @@ if __name__ == "__main__":
         # inquire if you mant to train or not    
         print("Base_dir:", BASE_DIR)
 
-        # if "annotations/person_keypoints_default.json" and "images" are not in BASE_DIR then 
+        # if LABEL_DIR and "images" are not in BASE_DIR then 
         # stop the program
 
-        if not os.path.exists(os.path.join(BASE_DIR, "annotations/person_keypoints_default.json")):
+        if not os.path.exists(os.path.join(BASE_DIR, LABEL_DIR)):
             print("annotations/person_keypoints_default.json is not found in BASE_DIR: ", BASE_DIR)
             exit(1)
 
@@ -491,8 +487,8 @@ if __name__ == "__main__":
             exit(1)
 
         cfg = init_keypoints(DATA_SET_NAME,
-                             fullpath_coco_train_json=os.path.join(BASE_DIR, "annotations/person_keypoints_default.json"),
-                             fullpath_coco_train_images=os.path.join(BASE_DIR,"images"))
+                         fullpath_coco_train_json=os.path.join(BASE_DIR, LABEL_DIR),
+                         fullpath_coco_train_images=os.path.join(BASE_DIR, IMAGE_DIR))
 
         if INTERACTIVE:                             
             print("Do you want to train the model? **Previous results might be gone without backup** (y/n)")                   
@@ -501,7 +497,16 @@ if __name__ == "__main__":
             answer = "y"
         
         if answer == "y":
-            train(DATA_SET_NAME, cfg, resume=False, model_weight=K101, config_file=K101)
+
+            train(cfg, train_dataset=None, test_dataset=None , resume=False, model_weight=None, config_file=TRAIN_WEIGHT_CONF)
+    
+            # launch(
+            #     train_from_zero,
+            #     4,
+            #     num_machines = 1,
+            #     machine_rank = 0,
+            #     dist_url = 'auto'
+            #     )
         else:
             print("No training is done.")
 
